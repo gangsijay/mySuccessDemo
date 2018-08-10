6539c9fb86db54536d17ec7db08ab98ad6454171
gangsijay <8gangsi@163.com>
2018-08-10 15:34:52 +0800

    Spring 5.0+Spring Boot+security+spring cloud oauth2+Redis 第一个完整例子

---
+++ LG-DEMO1/zuul-server/src/main/java/com/two/zuulserver/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.two.zuulserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by SuperS on 2017/9/25.
+ *
+ * @author SuperS
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//密码加密器
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+    
+}
